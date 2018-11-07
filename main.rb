18ede02d1d3a66af5d154f178f103f637c0b5934
dimkawp <bondar.dmitriy.fmc@gmail.com>
2018-11-07 20:53:00 +0200

    constraction

---
+++ main.rb
@@ -0,0 +1,15 @@
+class MyPat << self
+    def name
+    end
+    def stats
+        {
+            name: '',
+            level: '',
+            body: '',
+            fanny: '',
+            total_balans: ''
+        }
+    end
+    def random_chaleng
+    end
+end
